ff9c58bc39c6859561562c3e30bad4a17227c323
Robert Hegeraad <roberthegeraad@gmail.com>
2014-09-07 21:44:49 +0200

    Added example file

--- example.php
+++ example.php
@@ -14,9 +14,10 @@ $client = new GuzzleHttp\Client();
 // 	'method' => 'get'
 // ));
 
-// var_dump($signature);
-
 // $response = $client->get('http://restwebservice.com/users/1?' . $signature);
+
+
+
 
 // // GET by ID
 // $signature = Signature::create(array(
@@ -28,6 +29,9 @@ $client = new GuzzleHttp\Client();
 // ));
 
 // $response = $client->get('http://restwebservice.com/users/1?' . $signature);
+
+
+
 
 // // POST
 // $signature = Signature::create(array(
@@ -47,6 +51,9 @@ $client = new GuzzleHttp\Client();
 // 	]
 // ]);
 
+
+
+
 // PUT
 $signature = Signature::create(array(
 	'public_key' => '761146773916773',
@@ -56,61 +63,14 @@ $signature = Signature::create(array(
 	'method' => 'delete'
 ));
 
-var_dump($signature);
-
 $response = $client->delete('http://restwebservice.com/users/761146773916776', [
     'body' => [
    		$signature
 	]
 ]);
 
-// $response = $client->put('http://restwebservice.com/users/1', array(
-// 	'Content-Length' => strlen($signature),
-// 	'Transfer-Encoding' => 'chunked'
-// ), array($signature));
-
-// $r = curl::put('http://restwebservice.com/users/1', array('dsad' => 'dsa'));
-
 echo '<pre>';
 var_dump($response->json());
 echo '</pre>';
 
-
-// http://jcalcote.wordpress.com/2008/10/16/put-or-post-the-rest-of-the-story/
-// Headers goed setten? content-length moet precies die length zijn van de body
-// via cURL proberen en dan custom cURL met guzzle
-
-
 ?>
-<!doctype html>
-<html>
-<head>
-	<title>REST</title>
-</head>
-<body>
-
-	<input class="btn" type="submit" value="Get Users"/>
-
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>
-
-	// $(document).on('click', '.btn', function()
-	// {
-	// 	$.ajax({
-	// 		type: 'GET',
-	// 		url: 'http://localhost:8080/Work/REST/users',
-	// 		data: {
-	// 			hash: '<?php echo $hash; ?>',
-	// 			id: '<?php echo $user_id; ?>',
-	// 			timestamp: '<?php echo time(); ?>'
-	// 		},
-	// 		success: function(r) {
-	// 			console.log(r);
-	// 		}
-	// 	})
-	// });
-	
-</script>
-</body>
-</html>
